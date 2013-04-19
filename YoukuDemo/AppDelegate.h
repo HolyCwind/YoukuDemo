ec4875a078df2aaac46c4036f3c85e6f9eabf717
HolyCwind <cwindbase@gmail.com>
2013-04-19 18:20:56 +0800

    Initial Commit

---
+++ YoukuDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YoukuDemo
+//
+//  Created by Cwind on 13-4-19.
+//  Copyright (c) 2013年 com.cwind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ YoukuDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YoukuDemo
+//
+//  Created by Cwind on 13-4-19.
+//  Copyright (c) 2013年 com.cwind. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
